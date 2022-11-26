1abe7b1e448bf98d84357eef3269b49962b0ab2d
Guilherme Amaral <gui.amaral123@outlook.com>
2022-11-26 11:50:15 -0300

    criando a lógica da aplicação

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ function tocar(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
+//lista de teclas
 const teclas = document.querySelectorAll('.tecla');
 
 let i = 0;
 
 while(i < teclas.length) {
 
-    teclas[i].onclick = tocar;
+    const tecla = teclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = () => {
+        tocar(idAudio);
+    };
 
     i++;
-
-    console.log(i);
 }
